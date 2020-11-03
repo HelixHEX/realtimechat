efd8a310204ca505deae40f8aaa9365d23af6bfe
HelixHEX <heyy.im.eli@gmail.com>
2020-11-03 18:46:15 -0500

    Removed Server folder

--- server/src/types.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-// import { Redis } from "ioredis";
-
-export type MyContext = {
-  req: Request;
-  // redis: Redis;
-  res: Response;
-};
--- server/src/utilities/MessageInput.ts
+++
@@ -1,9 +0,0 @@
-import { Field, InputType } from "type-graphql";
-
-@InputType()
-export class MessageInput {
-  @Field()
-  message: string;
-  @Field()
-  senderName: string;
-}
